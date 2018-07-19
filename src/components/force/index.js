ae3dcfcf2de335e185834f031deb691c85604211
stonerao <674656681@qq.com>
2018-07-19 22:36:13 +0800

    1
    Signed-off-by: stonerao <674656681@qq.com>

--- src/components/force/index.js
+++ src/components/force/index.js
@@ -1,7 +1,7 @@
 import * as d3 from "d3";
 
 let color = d3.scaleOrdinal(d3.schemeCategory20);
-var main 
+var main
 class render {
   constructor(gloal) {
     /**
@@ -17,10 +17,11 @@ class render {
       return
     }
     //选择svg并且赋予高宽
-    let [width, height] = [gloal.width || 500, gloal.height || 300]
-    var svg = d3.select(gloal.el)
-      .attr("width", width)
-      .attr("height", height);
+    let [width, height] = [
+      gloal.width || 500,
+      gloal.height || 300
+    ]
+    var svg = d3.select(gloal.el).attr("width", width).attr("height", height);
     //添加主体节点
     main = svg.append("g").attr("class", "main");
     if (gloal.zoom) {
@@ -35,50 +36,36 @@ class render {
 
     }
     //动力
-    var repelForce = d3
-      .forceManyBody()
-      .strength(-1500)
-      .distanceMax(800)
-      .distanceMin(0);
-    var simulation = d3
-      .forceSimulation()
-      .force("link", d3.forceLink().id(function (d) {
-        return d.id;
-      }))
-      .force("manyBody", repelForce)
-      .force("center", d3.forceCenter(width / 2, height / 2))
-      .on("tick", ticked);
-    var link = main
-      .append("g")
-      .attr("class", "links")
-      .selectAll("line")
-    var node = main
-      .append("g")
-      .attr("class", "nodes")
-      .selectAll("circle");
-    var t = d3
-      .transition()
-      .duration(50)
-      .ease(d3.easeLinear);
+    var repelForce = d3.forceManyBody().strength(-1500).distanceMax(800).distanceMin(0);
+    var simulation = d3.forceSimulation().force("link", d3.forceLink().id(function(d) {
+      return d.id;
+    })).force("manyBody", repelForce).force("center", d3.forceCenter(width / 2, height / 2)).on("tick", ticked);
+    var link = main.append("g").attr("class", "links").selectAll("line")
+    var node = main.append("g").attr("class", "nodes").selectAll("circle");
+    var textNodes = main.append("g").attr("class", "text");
+
+    var t = d3.transition().duration(50).ease(d3.easeLinear);
     //ticked event
     function ticked() {
-      link
-        .attr("x1", function (d) {
-          return d.source.x;
-        })
-        .attr("y1", function (d) {
-          return d.source.y;
-        })
-        .attr("x2", function (d) {
-          return d.target.x;
-        })
-        .attr("y2", function (d) {
-          return d.target.y;
-        });
+      link.attr("x1", function(d) {
+        return d.source.x;
+      }).attr("y1", function(d) {
+        return d.source.y;
+      }).attr("x2", function(d) {
+        return d.target.x;
+      }).attr("y2", function(d) {
+        return d.target.y;
+      });
 
-      node.attr("transform", function (d) {
+      node.attr("transform", function(d) {
         return "translate(" + d.x + "," + d.y + ")";
       });
+      textNodes.attr("x",d=>{
+        return (d.source.x+d.target.x)/2
+      })
+      textNodes.attr("y",d=>{
+        return (d.source.y+d.target.y)/2
+      })
     }
 
     function dragstarted(d) {
@@ -101,59 +88,45 @@ class render {
       d.fx = null;
       d.fy = null;
     }
-var _this = this;
+    var _this = this;
     function restart(data) {
       // Apply the general update pattern to the nodes.
-      node = _this.node().data(data.nodes, function (d) {
+      node = _this.node().data(data.nodes, function(d) {
         return d.id;
       });
       node.exit().remove();
-      let allG = node
-        .enter()
-        .append("g")
-        .call(
-          d3
-          .drag()
-          .on("start", dragstarted)
-          .on("drag", dragged)
-          .on("end", dragended)
-        );
+      let allG = node.enter().append("g").call(d3.drag().on("start", dragstarted).on("drag", dragged).on("end", dragended));
       let size = 15;
-      var newNode = allG
-        .append("circle")
-        .attr("r", size)
-        .style("opacity", 0)
-        .attr("fill", d => {
-          return color(d.group);
-        });
+      var newNode = allG.append("circle").attr("r", size).style("opacity", 0).attr("fill", d => {
+        return color(d.group);
+      });
 
-      var texts = allG
-        .append("text")
-        .text(function (d) {
-          return d.id;
-        })
-        .attr("text-anchor", "middle")
-        .attr("y", `6px`);
+      var texts = allG.append("text").text(function(d) {
+        return d.id;
+      }).attr("text-anchor", "middle").attr("y", `6px`);
       newNode.transition(t).style("opacity", 1);
       node = allG.merge(node);
 
-      newNode.append("title").text(function (d) {
+      newNode.append("title").text(function(d) {
         return d.id + " (" + d.group + ")";
       });
       // Apply the general update pattern to the links.
-      link = link.data(data.links, function (d) {
+      link = link.data(data.links, function(d) {
         return d.source.id + "-" + d.target.id;
       });
       link.exit().remove();
-      link = link
-        .enter()
-        .append("line")
-        .attr("stroke-width", function (d) {
-          return Math.sqrt(d.value);
-        }).on("click", function (data) {
-          console.log(data)
-        })
-        .merge(link);
+      link = link.enter().append("line").attr("stroke-width", function(d) {
+        return Math.sqrt(d.value);
+      }).on("click", function(data) {
+        console.log(data)
+      }).merge(link);
+
+      textNodes.exit().remove();
+      textNodes = textNodes.selectAll("g").data(data.links).enter().append("g").append("text").text(function(e) {
+        console.log(e.source.id)
+        return e.value+"111111"
+      }).attr("text-anchor", "middle")
+
       simulation.nodes(data.nodes);
       simulation.force("link").links(data.links);
       simulation.alpha(1).restart();
@@ -161,9 +134,7 @@ var _this = this;
     restart(gloal.data)
     this.restart(gloal.data)
   }
-  restart() {
-
-  }
+  restart() {}
   node() {
     return main.append("g").attr("class", "nodes").selectAll("circle");
   }
@@ -176,7 +147,5 @@ var _this = this;
     return color(value)
   }
 }
-
-
 
 export default render
